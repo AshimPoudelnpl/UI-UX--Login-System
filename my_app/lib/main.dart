e4ae1358f20be0ea102eb9114468190eccd88dc4
AshimPoudelnpl <ashimpdl7@gmail.com>
2026-09-01 21:44:47 +0545

    Refactor code structure and remove redundant sections

---
+++ my_app/lib/main.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+import 'package:my_app/screens/LoginScreen.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
